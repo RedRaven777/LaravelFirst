db48a30bd05b6df5b645c11ff21fa68592e87c70
Raven <flameraven.pro@gmail.com>
2020-10-24 02:37:35 +0300

    Install and settings Npm Sass, create basic user registration

---
+++ resources/views/auth/verify.blade.php
@@ -0,0 +1,65 @@
+@extends('main_template')
+
+@section('title') <title>Verify</title> @endsection
+
+@section('nav-list')
+<ul class="nav nav-pills">
+    <li role="presentation"><a href="/">Home</a></li>
+    @guest
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+        </li>
+        @if (Route::has('register'))
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+            </li>
+        @endif
+    @else
+        <li class="nav-item dropdown">
+            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="/home" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                {{ Auth::user()->name }}
+            </a>
+        </li>
+        <li class="nav-item">
+            <!-- <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown"> -->
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                                 document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            <!-- </div> -->
+        </li>
+    @endguest
+</ul>
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+
+                <div class="card-body">
+                    @if (session('resent'))
+                        <div class="alert alert-success" role="alert">
+                            {{ __('A fresh verification link has been sent to your email address.') }}
+                        </div>
+                    @endif
+
+                    {{ __('Before proceeding, please check your email for a verification link.') }}
+                    {{ __('If you did not receive the email') }},
+                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                        @csrf
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- webpack.mix.js
+++ webpack.mix.js
@@ -6,12 +6,33 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+	.version()
+    .sass('resources/sass/app.sass', 'public/css')
+    .options({
+        processCssUrls: true
+    });
+
+// var browserSync = require("browser-sync").create();
+
+// *
+//  * This example will serve files from the './app' directory
+//  * and will automatically watch for html/css/js changes
+ 
+// browserSync.init({
+// 	watch: true,
+// 	online: true,
+// 	tunnel: true,
+// 	injectChanges: false,
+// 	server: ["resources", "public"]
+// });
+
+
+mix.browserSync({
+   proxy: "resources"
+});
